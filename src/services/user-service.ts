772f272093e3cbe464997aa2cbd61af6c7cd02c7
anshul.a.nema <anshul.a.nema@accenture.com>
2020-04-14 01:14:44 +0530

    bas64 changes

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,29 +1,28 @@
-
-const Base64 = require('js-base64');
+var base64 = require("base-64");
 export interface User {
-    email: string;
-    password: string;
-    role: string;
+  email: string;
+  password: string;
+  role: string;
 }
 
-const UUID: string = 'uuID'
+const UUID: string = "uuID";
 
 export class UserService {
-    public static async login(user:User) {
-        sessionStorage.setItem(UUID, Base64.encode(JSON.stringify((user))));
-        return true;
-    }
+  public static async login(user: User) {
+    sessionStorage.setItem(UUID, base64.encode(JSON.stringify(user)));
+    return true;
+  }
 
-    public static logout(): void {
-        sessionStorage.removeItem(UUID);
-    }
+  public static logout(): void {
+    sessionStorage.removeItem(UUID);
+  }
 
-    public static isUserLoggedIn(): boolean {
-        return sessionStorage.getItem(UUID) ? true : false;
-    }
+  public static isUserLoggedIn(): boolean {
+    return sessionStorage.getItem(UUID) ? true : false;
+  }
 
-    public static getUser(): User {
-        const user: any = sessionStorage.getItem(UUID);
-        return JSON.parse(Base64.decode(user));
-    }
+  public static getUser(): User {
+    const user: any = sessionStorage.getItem(UUID);
+    return JSON.parse(base64.decode(user));
+  }
 }
