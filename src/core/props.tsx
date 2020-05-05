99a91454c5a1b63654e46558e16121dfad03865d
nemaanshul <anshulnema@live.com>
2020-05-05 22:28:44 +0530

    changes for meta tags in index.html

--- src/core/props.tsx
+++ src/core/props.tsx
@@ -1,57 +1,66 @@
-import { IIndexable } from '../services/http-service';
+import { IIndexable } from "../services/http-service";
 export interface Media {
-    title:string;
-    type:string;
-    url:string;
+  title: string;
+  type: string;
+  url: string;
 }
 
 export interface PointTable {
-    id: string;
-    name: string;
-    data: Array<any>;
+  id: string;
+  name: string;
+  data: Array<any>;
 }
 
 export interface CardDetail {
-    url?: string;
-    title?: string;
-    description?: string;
-    date?: string;
-    links?: Array<any>;
+  url?: string;
+  title?: string;
+  description?: string;
+  date?: string;
+  links?: Array<any>;
 }
 
 export interface Team {
-    name?: string;
-    url?: string;
-    id?: string;
-    players?: Player[];
+  name?: string;
+  url?: string;
+  id?: string;
+  players?: Player[];
 }
 
 export interface Player {
-    name?: string;
-    role?: string;
+  name?: string;
+  role?: string;
 }
 
 export interface Tournament {
-    name?: string;
-    id?: string;
-    teams? : Team[];
+  name?: string;
+  id?: string;
+  teams?: Team[];
 }
 
 export interface BarInterface {
-    key?: number;
-    x?: any;
-    y? : any;
-    width? : number;
-    height? : number;
+  key?: number;
+  x?: any;
+  y?: any;
+  width?: number;
+  height?: number;
+}
+
+export interface IFbMetaTags {
+  description?: string;
+  link?: string;
+  title?: string;
+  image?: string;
 }
 
 export type AppProps = {
-    allowedPointHeaders?: string[];
-    medias?: Media[];
-    pointTables? : PointTable[];
-    tournaments?: Tournament[];
-    cardDetails?: CardDetail[];
-    eventDetails?: CardDetail[];
-    chartData?: any;
-    barInterface?: BarInterface;
-}
+  allowedPointHeaders?: string[];
+  medias?: Media[];
+  pointTables?: PointTable[];
+  tournaments?: Tournament[];
+  cardDetails?: CardDetail[];
+  eventDetails?: CardDetail[];
+  chartData?: any;
+  barInterface?: BarInterface;
+  hasShareLink?: boolean;
+  fbMetaTags?: IFbMetaTags;
+};
--- src/routes/events.tsx
+++ src/routes/events.tsx
@@ -1,26 +1,29 @@
 import React, { FC, useContext } from "react";
 import { AppProps } from "../core/props";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 import { AppCard } from "../components";
-import { ApiContext } from '../core/api-context';
+import { ApiContext } from "../core/api-context";
 
 const Events: FC<AppProps> = (props: AppProps) => {
-    const context: any = useContext(ApiContext);
-    console.log(context.eventDetails, 'context.eventDetails')
-    const slicedCardDetails = context.cardDetails.slice(1, context.cardDetails.lenght);
-    return (
-        <Container>
-            <Row className="mt-5">
-                <Col md="8">
-                    <Row>
-                        <AppCard cardDetails={context.eventDetails} />
-                    </Row>
-                </Col>
-                <Col md="4">
-                    <AppCard cardDetails={slicedCardDetails} />
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+  const context: any = useContext(ApiContext);
+  console.log(context.eventDetails, "context.eventDetails");
+  const slicedCardDetails = context.cardDetails.slice(
+    1,
+    context.cardDetails.lenght
+  );
+  return (
+    <Container>
+      <Row className="mt-5">
+        <Col md="8">
+          <Row>
+            <AppCard hasShareLink={true} cardDetails={context.eventDetails} />
+          </Row>
+        </Col>
+        <Col md="4">
+          <AppCard cardDetails={slicedCardDetails} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default Events;
